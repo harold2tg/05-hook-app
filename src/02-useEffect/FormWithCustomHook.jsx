1ea8cdca3cb91380658c12ee4c94f6aa71c891fa
Harold Torres Gallo <harold2tg@gmail.com>
2022-10-13 17:54:33 -0500

    122. Formulario con custom Hook forma1

---
+++ src/02-useEffect/FormWithCustomHook.jsx
@@ -0,0 +1,47 @@
+import { useEffect } from "react";
+import { useForm } from "../hook/useForm";
+export const FormWithCustomHook = () => {
+
+  const { formState, onInputChange} = useForm(
+    {
+      username:'',
+      email:'',
+      password: ''
+  
+    });
+
+
+    const {username, email,password} = formState;
+  
+
+  
+  return (
+    <>
+    <h1>Formulario con custom hook</h1>
+    <hr />
+    <input type="text"
+           className="form-control"
+           placeholder="Username"
+           name="username"
+           value={username}
+           onChange={onInputChange}
+           />
+    <input type="text"
+           className="form-control mt-2"
+           placeholder="alguien@alguien.com"
+           name="email"
+           value={email}
+           onChange={onInputChange}
+           />       
+    <input type="password"
+           className="form-control mt-2"
+           placeholder="contraseña"
+           name="password"
+           value={password}
+           onChange={onInputChange}
+           />       
+           
+    </>    
+  )
+}
+
--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,13 @@ import './index.css'
 import { CounterApp } from './01-useState/CounterApp'
 import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 import {SimpleForm} from './02-useEffect/SimpleForm'
+import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    {/* <HookApp /> */}
-    {/* <CounterApp/> */}
-    {/* <CounterWithCustomHook/> */}
-    <SimpleForm/>
-  </React.StrictMode>
+    // {/* <HookApp /> */}
+    // {/* <CounterApp/> */}
+    // {/* <CounterWithCustomHook/> */}
+    // <SimpleForm/>
+
+    <FormWithCustomHook/>
 )
