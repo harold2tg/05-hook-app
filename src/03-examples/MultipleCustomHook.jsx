1f7769cbd5127c7bd2cbdb443a70d22a1480d70b
Harold Torres Gallo <harold2tg@gmail.com>
2022-10-18 09:57:09 -0500

    125. useFetch + useCounter

---
+++ src/03-examples/MultipleCustomHook.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { useFetch } from '../hook/useFetch'
+
+export const MultipleCustomHook = () => {
+    const {data,isLoading,hasError} = useFetch('https://www.breakingbadapi.com/api/quotes/1')
+    console.log({data, isLoading,hasError})
+  return (
+        <>
+        <h1>BreakingBad Quotes</h1>
+        <hr />
+        {
+            (isLoading)
+            ?(
+                <div className="alert alert-info text-cennter">
+                loading
+            </div>   
+            ):(
+                <blockquote className='blockquote text-end'>
+                    <p className='mb-2'>{data[0].quote}</p>
+                    <footer className='blockquote-footer'>
+                        {data[0].author}
+                    </footer>
+                </blockquote>     
+            )
+        }
+        
+        
+        </>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,18 @@ import { CounterApp } from './01-useState/CounterApp'
 import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 import {SimpleForm} from './02-useEffect/SimpleForm'
 import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
+import { MultipleCustomHook } from './03-examples/MultipleCustomHook'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+//    <React.StrictMode>
     // {/* <HookApp /> */}
     // {/* <CounterApp/> */}
     // {/* <CounterWithCustomHook/> */}
     // <SimpleForm/>
 
-    <FormWithCustomHook/>
+    // <FormWithCustomHook/>
+
+    <MultipleCustomHook/>
+    // </React.StrictMode>
+
 )
