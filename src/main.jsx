1d912f0d5c4b81419d71af82fca2daf0e14b2da2
Harold Torres Gallo <harold2tg@gmail.com>
2022-10-21 11:32:33 -0500

    139. Continuación del Proyecto - HookApp

--- src/main.jsx
+++ src/main.jsx
@@ -13,6 +13,7 @@ import { Memorize } from './06-memos/Memorize'
 import { MemoHook } from './06-memos/MemoHook'
 import { CallbackHook } from './06-memos/CallbackHook'
 import { Padre } from './07-tarea-memo/Padre'
+import  './08-useReducer/intro-reducer'
 
 
 
